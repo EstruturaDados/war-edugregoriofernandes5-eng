3c05d8e00f629b475a67a1ccd5514b5f4970e87e
edugregoriofernandes5-eng <edugregoriofernandes5@gmail.com>
2025-09-18 20:34:57 +0000

    Modifiquei o arquivo batalha.c

--- batalha.c
+++ batalha.c
@@ -2,4 +2,35 @@
 
 int main(){
     printf("desafio war|\n");
+    printf("batalha.c|\n");
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_NOME 30
+#define MAX_COR 20
+
+ // Estrutura Territorio
+ typedef struct{
+    char nome[MAX_NOME];         // Nome do territorio
+    char corExercito[MAX_COR];  // Cor do exercito que ocupa
+    int tropas;                 //Quantidade de tropas  
+    }Territorio;
+
+    //======== Funções ========
+
+    //Cadastro dinâmico
+    Territorio* cadastrarTerritorios(int n){
+        Territorio* territorios =(Territorio*) calloc(n, sizeof(Territorio));
+        if (!territorios) {
+            printf ("Erro ao alocar memória!\n");
+            exit(1);
+        }
+
+        printf("n=== Cadastro de Territórios ===\n\n");
+        
+
+
