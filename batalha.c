88e1ab50bda5ce90edd1cd3354a01697049e7e80
edugregoriofernandes5-eng <edugregoriofernandes5@gmail.com>
2025-09-30 11:00:09 +0000

    Modifiquei o arquivo

--- batalha.c
+++ batalha.c
@@ -1,4 +1,11 @@
+// war_sim.c
+// Simulação simples de ataque entre territórios usando alocação dinâmica e ponteiros.
+// Compilar: gcc -o war_sim war_sim.c
+// Rodar: ./war_sim
+
 #include <stdio.h>
+#include <stdlib.h>
+
 
 
 
