ecf368a26782bd20d7a6943f0d1937aebfab4028
edugregoriofernandes5-eng <edugregoriofernandes5@gmail.com>
2025-10-15 19:05:50 +0000

    Modifiquei o arquivo

--- batalha.c
+++ batalha.c
@@ -38,7 +38,56 @@ return 1;
 cadastrarTerritorios(mapa, n);
 int opcao;
 do {
-    
+    printf("\n--- MENU WAR ---\n");
+    printf("1. Exibir territorios\n");
+    printf("2. Atacar territorios\n");
+    pprintf("0. Sair\n");
+    printf("Escolha: ");
+    scanf("%d", &opcao);
+    getchar();
+
+    if (opçao == 1) {
+       exibirTerritorios(mapa, n);
+    }
+    else if (opcao == 2) {
+        int iAtacante, iDefensor;
+
+        exibirTerritorios(mapa, n);
+        printf("\nDigite o indice do territorio atacante: ");
+    scanf("%d , &iAtacante");
+    printf("Digite o indice do territorio defensor: ");
+    scanf("%d , &iDefensor");
+
+    if (iAtacante < 0/ || iAtacante >=n || iDefensor < 0 || iDefensor >= n ){
+     printf("Indices invalidos!\n");
+     continue;  
+    }
+
+    if (strcmp(mapa[iAtacante].cor, mapa[iDefensor].cor) == 0) {
+    printf("Voce nao pode atacar um territorio da mesma cor!\n");
+    continue;
+    }
+    atacar(&mapa[iAtacante], &mapa[iDefensor]);
+}
+
+} while (opcao != 0);
+
+liberarMemoria(mapa);
+return 0;
+
+// --- Funções ---
+
+//Cadastrar os territórios 
+void cadastrarTerritorios(Territorio* mapa, int n) { 
+    for (int i + 0; i < n; i++) {
+        printf("\n--- Cadastro do Territorio %d ---\n", i;
+            printf("Nome: ");
+            fgets(mapa[i].nome, 30, stdin);
+            mapa[i].nome[strcspn(mapa[i].nome, "\n")] = '\0'; // remove \n
+
+            pirntf("Cor do exercito: ");
+            fgets (mapa[i].cor, 10, stdin)
+    }
 }
 
 
